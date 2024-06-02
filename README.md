dbb8ca83a70724010257b3d1bbe178f4b96c4945
Harsh-Thanki <thankiharsh7529@gmail.com>
2024-06-02 19:04:00 +0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # mp3_to_text
+
+1. Activate your virtual environment.
+
+2. Install whisper from it's repository:
+
+```sh
+pip install git+https://github.com/openai/whisper.git
+```
+
+3. Install ffmpeg
+
+```sh
+sudo apt update
+sudo apt install ffmpeg
+```
+
+4. Create a blank txt in same directory as the python file.
+5. Replace the file name to newly created file name at output_file_path.
+6. Run the main.py
---
+++ main.py
@@ -0,0 +1,25 @@
+import whisper
+
+print("Model is being loaded.")
+# Loading the Whisper model: options are tiny, base, small, medium, large
+model = whisper.load_model("tiny")
+print("Model loaded!!")
+
+# Transcribing the MP3 file
+audio_file_path = "Kwaai Public Meeting May 17th 2024.mp3"  # MP3 file path
+result = model.transcribe(audio_file_path)
+
+# Extracting the transcription
+transcription = result['text']
+
+# Printing the transcription to the console
+print(transcription)
+
+# Saving the transcription to a text file
+output_file_path = "transcription tiny model.txt"  # Replace with your desired output file path
+with open(output_file_path, 'w') as file:
+    file.write(transcription)
+
+print(f"Transcription saved to {output_file_path}")
+
+
