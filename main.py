56666591b7a70782584520ae840500a5fd02bb35
Harsh-Thanki <thankiharsh7529@gmail.com>
2024-06-10 18:39:55 +0400

    paths updated.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ model = whisper.load_model("tiny")
 print("Model loaded!!")
 
 # Transcribing the MP3 file
-audio_file_path = "Kwaai Public Meeting May 17th 2024.mp3"  # MP3 file path
+audio_file_path = "input/Kwaai Public Meeting May 17th 2024.mp3"  # MP3 file path
 result = model.transcribe(audio_file_path)
 
 # Extracting the transcription
@@ -16,7 +16,7 @@ transcription = result['text']
 print(transcription)
 
 # Saving the transcription to a text file
-output_file_path = "transcription tiny model.txt"  # Replace with your desired output file path
+output_file_path = "output/transcription.txt"  # Replace with your desired output file path
 with open(output_file_path, 'w') as file:
     file.write(transcription)
 
